97202d3a18b744719c9901ca1afd49f7aaa7e85e
uniqueding <1224472370@qq.com>
2021-07-24 15:22:49 +0800

    add i18n

--- src/main.js
+++ src/main.js
@@ -4,5 +4,6 @@ import App from './App.vue'
 //bootstrap
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle.js'
+import i18n from './i18n'
 
-createApp(App).mount('#app')
+createApp(App).use(i18n).mount('#app')
